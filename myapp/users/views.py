15285e1d5ec8b401d84244da7b18bb4f12794e34
Bharat <bharat1910109@akgec.ac.in>
2023-04-21 15:58:57 +0530

    second commit

--- myapp/users/views.py
+++ myapp/users/views.py
@@ -7,7 +7,7 @@ from .models import Customusers
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required
+
 def index(request):
     return render(request,"users/index.html")
 
